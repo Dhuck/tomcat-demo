b9863a1470e2e898afea59cf86ae29f8f9307027
hpolanco29 <hpolanco@iwa.com.mx>
2017-10-05 23:04:08 -0500

    Initial commit. . .

---
+++ src/main/java/mx/sismo/api/web/UserRestController.java
@@ -0,0 +1,18 @@
+package mx.sismo.api.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserRestController {
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<Void> retrieveUser() {
+
+		return new ResponseEntity<Void>(HttpStatus.OK);
+	}
+}
