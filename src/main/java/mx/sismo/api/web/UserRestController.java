27eb77ae0ce9e5b0ef44ec73d6b72d0278fccf17
Dhuck <hectornet80@gmail.com>
2022-03-15 22:24:22 -0600

    Update UserRestController.java

--- src/main/java/mx/sismo/api/web/UserRestController.java
+++ src/main/java/mx/sismo/api/web/UserRestController.java
@@ -12,6 +12,13 @@ public class UserRestController {
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<Void> retrieveUser() {
+		
+		int target = -5;
+                int num = 3;
+
+                target =- num;  // Noncompliant; target = -3. Is that really what's meant?
+                target =+ num; // Noncompliant; target = 3
+		
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 }
